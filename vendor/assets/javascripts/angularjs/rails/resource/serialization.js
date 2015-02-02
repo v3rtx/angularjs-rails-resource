c386e430b6fad93ffeb256be81ef1946e0dcd51d
andrey <andrey@rademade.com>
2015-02-02 13:52:15 +0200

    fixing bugs

--- vendor/assets/javascripts/angularjs/rails/resource/serialization.js
+++ vendor/assets/javascripts/angularjs/rails/resource/serialization.js
@@ -304,8 +304,8 @@
                     var camelizedName = this.camelize(attributeName);
 
                     camelizedName = this.deserializeMappings[attributeName] ||
-                        this.deserializeMappings[camelizedName] ||
-                        camelizedName;
+                    this.deserializeMappings[camelizedName] ||
+                    camelizedName;
 
                     if (this.isExcludedFromDeserialization(attributeName) || this.isExcludedFromDeserialization(camelizedName)) {
                         return undefined;
@@ -353,7 +353,7 @@
                  * @param data The data to prepare
                  * @returns {*} A new object or array that is ready for JSON serialization
                  */
-                Serializer.prototype.deserializeData = function (data, Resource) {
+                Serializer.prototype.serializeValue = function (data) {
                     var result = data,
                         self = this;
 
@@ -361,7 +361,7 @@
                         result = [];
 
                         angular.forEach(data, function (value) {
-                            result.push(self.deserializeData(value, Resource));
+                            result.push(self.serializeValue(value));
                         });
                     } else if (angular.isObject(data)) {
                         if (angular.isDate(data)) {
@@ -369,26 +369,15 @@
                         }
                         result = {};
 
-                        if (Resource) {
-                            result = new Resource.config.resourceConstructor();
-                        }
-
-                        this.deserializeObject(result, data);
-
+                        angular.forEach(data, function (value, key) {
+                            // if the value is a function then it can't be serialized to JSON so we'll just skip it
+                            if (!angular.isFunction(value)) {
+                                self.serializeAttribute(result, key, value);
+                            }
+                        });
                     }
 
                     return result;
-                };
-
-                Serializer.prototype.deserializeObject = function (result, data) {
-                    //console.log(data);
-                    //console.log(Resource);
-
-                    var $this = this;
-                    angular.forEach(data, function (value, key) {
-                        $this.deserializeAttribute(result, key, value);
-                    });
-                    return data
                 };
 
                 /**
@@ -459,20 +448,30 @@
                         if (angular.isDate(data)) {
                             return data;
                         }
-
                         result = {};
 
                         if (Resource) {
                             result = new Resource.config.resourceConstructor();
                         }
 
-                        angular.forEach(data, function (value, key) {
-                            self.deserializeAttribute(result, key, value);
-                        });
+                        this.deserializeObject(result, data);
+
                     }
 
                     return result;
                 };
+
+                Serializer.prototype.deserializeObject = function (result, data) {
+                    //console.log(data);
+                    //console.log(Resource);
+
+                    var tthis = this;
+                    angular.forEach(data, function (value, key) {
+                        tthis.deserializeAttribute(result, key, value);
+                    });
+                    return data;
+                };
+
 
                 /**
                  * Transforms an attribute and its value and stores it on the parent data object.  The attribute will be
